bb531bfc2085aed673831ed9a0155220254f0e7d
Šimun Strukan <simun.strukan@gmail.com>
2017-08-07 03:15:17 +0200

    Second stage work on api.module

--- models/account.rb
+++ models/account.rb
@@ -1,10 +1,13 @@
 class Account 
 	include DataMapper::Resource
 
-	property :id, 					 Serial
-  property :name, 			   String,   :length => 255
-  property :email, 		   	 String,   :length => 255
-  property :created_on,		 DateTime 
+	property :id, 				Serial
+  property :name, 			String,  :length => 255
+  property :email, 		  String,  :length => 255, :unique => true, :format => :email_address
+  property :created_at, DateTime
+	property :created_on, Date   
+	property :updated_at, DateTime
+  property :updated_on, Date
 
   belongs_to :user
 end
--- models/user.rb
+++ models/user.rb
@@ -2,12 +2,19 @@ class User
   include DataMapper::Resource
 
   property :id, 					 Serial
-  property :email, 				 String,   :length => 255
+  property :email, 				 String,   :length => 255, :unique => true, :format => :email_address
   property :password, 		 String,   :length => 255
   property :token, 				 String,   :length => 255
   property :recover_code,  String,   :length => 255
-  property :created_on, DateTime
+  property :created_at, 	 DateTime
+	property :created_on, 	 Date   
+	property :updated_at, 	 DateTime
+  property :updated_on, 	 Date
 
-  has 1, :account
+  has n, :accounts
+  has n, :hostings
+  has n, :attendings
+  has n, :events, :through => :hostings
+  has n, :events, :through => :attendings
   
 end
